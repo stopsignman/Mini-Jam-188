38062ac23dda56b4b15976f58e13e1433a053d25
stopsignman <maxwellj.kme@gmail.com>
2025-07-03 22:10:10 -0600

    Architecture setup
     - SaveManager script
     - Test and menu scenes

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
